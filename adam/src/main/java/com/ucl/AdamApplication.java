e0a64453fd3a37c817611b723c27fd6829b8b0b8
ramses <ramses69@naver.com>
2024-03-06 15:44:45 +0900

    first

---
+++ adam/src/main/java/com/ucl/AdamApplication.java
@@ -0,0 +1,13 @@
+package com.ucl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdamApplication.class, args);
+	}
+
+}
---
+++ adam/src/test/java/com/ucl/AdamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
