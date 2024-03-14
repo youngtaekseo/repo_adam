671bbe26ce5082d6232e8dc1cdd9e813183dbd56
ramses <ramess69@naver.com>
2024-03-14 17:49:33 +0900

    javascript 자동완성 패키지 적용

--- adam/src/main/java/com/ucl/infra/codegroup/CodeGroupDto.java
+++ adam/src/main/java/com/ucl/infra/codegroup/CodeGroupDto.java
@@ -8,82 +8,58 @@ public class CodeGroupDto {
 	private Date 	cdgRegDt;
 	private Date 	cdgUdtDt;
 	private Integer cdgDelNy;
-	
-	private String xnameType;
-	private String xdateType;
-	private String xdateFrom;
-	private String xdateTo;
-	
-	private Integer xcodeGroupCount;
+		
 	private Integer xcodeCount;
-	
+
 //	-----------
 	
 	public String getCdgSeq() {
 		return cdgSeq;
 	}
+
 	public void setCdgSeq(String cdgSeq) {
 		this.cdgSeq = cdgSeq;
 	}
+
 	public String getCdgName() {
 		return cdgName;
 	}
+
 	public void setCdgName(String cdgName) {
 		this.cdgName = cdgName;
 	}
+
 	public Date getCdgRegDt() {
 		return cdgRegDt;
 	}
+
 	public void setCdgRegDt(Date cdgRegDt) {
 		this.cdgRegDt = cdgRegDt;
 	}
+
 	public Date getCdgUdtDt() {
 		return cdgUdtDt;
 	}
+
 	public void setCdgUdtDt(Date cdgUdtDt) {
 		this.cdgUdtDt = cdgUdtDt;
 	}
+
 	public Integer getCdgDelNy() {
 		return cdgDelNy;
 	}
+
 	public void setCdgDelNy(Integer cdgDelNy) {
 		this.cdgDelNy = cdgDelNy;
 	}
-	public String getXnameType() {
-		return xnameType;
-	}
-	public void setXnameType(String xnameType) {
-		this.xnameType = xnameType;
-	}
-	public String getXdateType() {
-		return xdateType;
-	}
-	public void setXdateType(String xdateType) {
-		this.xdateType = xdateType;
-	}
-	public String getXdateFrom() {
-		return xdateFrom;
-	}
-	public void setXdateFrom(String xdateFrom) {
-		this.xdateFrom = xdateFrom;
-	}
-	public String getXdateTo() {
-		return xdateTo;
-	}
-	public void setXdateTo(String xdateTo) {
-		this.xdateTo = xdateTo;
-	}
-	public Integer getXcodeGroupCount() {
-		return xcodeGroupCount;
-	}
-	public void setXcodeGroupCount(Integer xcodeGroupCount) {
-		this.xcodeGroupCount = xcodeGroupCount;
-	}
+
 	public Integer getXcodeCount() {
 		return xcodeCount;
 	}
+
 	public void setXcodeCount(Integer xcodeCount) {
 		this.xcodeCount = xcodeCount;
 	}
 	
+
 }
