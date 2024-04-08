d652481b8e8ac0dc065dcfc943bda550a4532152
ramses <ramses69@naver.com>
2024-04-08 18:43:07 +0900

    메인화면 작업중

--- adam/src/main/java/com/ucl/infra/product/ProductController.java
+++ adam/src/main/java/com/ucl/infra/product/ProductController.java
@@ -173,7 +173,10 @@ public class ProductController {
 
 	// 사용자 상세화면
 	@RequestMapping(value = "/productUsrDetail")
-	public String productUsrDetail(ProductVo vo, ReviewVo rvo, Model model) throws Exception {
+	public String productUsrDetail(ProductVo vo, ReviewVo rvo, Model model, HttpSession httpSession) throws Exception {
+		
+		// 로그인 회원순번 설정
+		vo.setShMbrSeq((String) httpSession.getAttribute("sessMbrSeq"));
 		
 		// 상세조회
 		model.addAttribute("item", service.selectListCarInfo(vo));
