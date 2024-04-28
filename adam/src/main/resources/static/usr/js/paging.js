402ddf8706ddae6daf95e4e51eceec674866d152
ramses <ramses69@naver.com>
2024-04-28 14:56:22 +0900

    차량조회:페이징처리시 조회조건 처리되도록 수정

--- adam/src/main/resources/static/usr/js/paging.js
+++ adam/src/main/resources/static/usr/js/paging.js
@@ -1,5 +1,8 @@
 // 사용자용 페이징
 fnGoList = function(pgPage) {
+	// 조회조건:차종, 제조사, 연식, 색상
+	fnCheckboxClick();
+	
     document.querySelector("input[name=pgPage]").value = pgPage;
     form.action = goUrlUsrList;
     form.submit();
