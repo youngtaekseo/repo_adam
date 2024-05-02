5638c90481d4c96dd14ec1eef8a1222b44b46721
ramses <ramses69@naver.com>
2024-05-02 11:31:11 +0900

    지도표시

--- adam/src/main/resources/static/sdm/js/common.js
+++ adam/src/main/resources/static/sdm/js/common.js
@@ -115,3 +115,58 @@ function execDaumPostcode() {
         }
     }).open();
 }
+
+// 지도보기
+function mapDisplay(traffic) {
+	let lat = $("#mbrPointLat").val();
+	let lon = $("#mbrPointLon").val();
+	
+	if(lat == null || lat == "" || lon == null || lon == "") {
+		$("#btnMapValid").css("visibility", "");
+		$("#btnMapValid").text("좌표값이 없습니다. 주소 검색 후 진행 바랍니다.");
+		$("#map").css("display", "none");
+		return;
+	}
+	
+	$("#trafficLabel").css("display", "");
+	$("#map").css("display", "");
+	
+	var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+	    mapOption = { 
+	        center: new kakao.maps.LatLng(lat, lon), // 지도의 중심좌표
+	        level: 3 // 지도의 확대 레벨
+	    };
+	
+	// 지도를 생성합니다
+	var map = new kakao.maps.Map(mapContainer, mapOption); 
+	
+	// 지도에 교통정보를 표시하도록 지도타입을 추가합니다
+	if(traffic == true){
+		map.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);  
+	}
+	
+	// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+	var mapTypeControl = new kakao.maps.MapTypeControl();
+	
+	// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+	// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+	map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+	
+	// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+	var zoomControl = new kakao.maps.ZoomControl();
+	map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+	
+	// 마우스 휠로 지도 확대,축소 가능여부를 설정합니다
+	map.setZoomable(false);   
+	
+	// 마커가 표시될 위치입니다 
+	var markerPosition  = new kakao.maps.LatLng(lat, lon); 
+	
+	// 마커를 생성합니다
+	var marker = new kakao.maps.Marker({
+	    position: markerPosition
+	});
+	
+	// 마커가 지도 위에 표시되도록 설정합니다
+	marker.setMap(map);		
+}
